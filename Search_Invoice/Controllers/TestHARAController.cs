4e11d466c52f1f05cfc0c690cd26a5efa383c905
NITRO-5\Huy <huydq@minvoice.vn>
2021-01-05 08:52:31 +0700

    Add project files.

---
+++ Search_Invoice/Controllers/TestHARAController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Http;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Search_Invoice.Controllers
+{
+    public class TestHARAController : Controller
+    {
+        //// GET: TestHARA
+        //public async Task<ActionResult> Index()
+        //{
+        //    using (var client = new HttpClient())
+        //    {
+        //        client.BaseAddress = new Uri("http://accounts.haravan.com/connect/authorize?response_mode=form_post&response_type=codeid_token & scope = openid profile email org userinfo & client_id = e5a87ceae07603e33fd5dc4e6b0118d5 & redirect_uri = http://localhost:57714/TestHARA/Index");
+        //        //var content = new FormUrlEncodedContent(new[]
+        //        //{
+        //        //new KeyValuePair<string, string>("", "login")
+        //        //});
+        //        var result = await client.GetAsync("http://accounts.haravan.com/connect/authorize?response_mode=form_post&response_type=codeid_token & scope = openid profile email org userinfo & client_id = e5a87ceae07603e33fd5dc4e6b0118d5 & redirect_uri = http://localhost:57714/TestHARA/Index");
+        //        string resultContent = await result.Content.ReadAsStringAsync();
+        //        Console.WriteLine(resultContent);
+        //    }
+
+        //    return View();
+
+        //}
+    }
+}
